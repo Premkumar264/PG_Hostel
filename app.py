5f52b7623bff50b4582357776d1dcea73e52b53d
Premkumar264 <157650600+Premkumar264@users.noreply.github.com>
2024-04-07 12:52:12 +0100

    Created get users API along with postgres database config

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
 
 app = Flask(__name__)
 
+# Connect to your PostgreSQL database
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="Prem@2604",
+    host="localhost"
+)
+cursor = conn.cursor(cursor_factory=RealDictCursor)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+# API to list all users
+@app.route('/users', methods=['GET'])
+def get_users():
+    cursor.execute("SELECT * FROM students")
+    students = cursor.fetchall()
+    return jsonify({'results': students})
+
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
