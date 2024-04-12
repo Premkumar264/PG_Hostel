9c8037f0c7cf52884991c98c0406d4f2ef134b17
PremkumarNeelam <20000202@mydbs.ie>
2024-04-12 11:41:11 +0100

    Created the service file for calling the API Request.

---
+++ pghostal_angular/src/app/user.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  baseUrl:string = 'http://127.0.0.1:5000'
+  jsonUrl:string = 'http://localhost:3000'
+
+  constructor(private http:HttpClient) { }
+
+  getUsers(){
+    return this.http.get(this.baseUrl + '/users')
+    // return this.http.get(this.jsonUrl + '/users')
+  }
+
+  getLogin(form:any){
+    return this.http.post(this.baseUrl + '/login',form)
+  }
+
+  addUser(form:any){
+    return this.http.post(this.baseUrl + '/users',form)
+
+  }
+
+  editUser(id:number,form:any){
+    return this.http.put(this.baseUrl + '/user/'+ id , form)
+    // return this.http.put(this.jsonUrl + '/users/'+ id , form)
+  }
+
+  deleteUser(id:number){
+    return this.http.delete(this.baseUrl + '/users/'+ id)
+  }
+
+}
