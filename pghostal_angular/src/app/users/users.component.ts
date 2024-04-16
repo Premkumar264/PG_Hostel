92f8eefb1e9545343cb3122828578eb00945b02a
PremkumarNeelam <20000202@mydbs.ie>
2024-04-16 11:24:44 +0100

    created users component  type script

--- pghostal_angular/src/app/users/users.component.ts
+++ pghostal_angular/src/app/users/users.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
+
+  users:any;
+  allUsers:any
+  searchTerm:any;
+  userForm!:FormGroup;
+  formTitle:string = '';
+
+  constructor(
+    private userService:UserService,
+    private router:Router,
+    private fb:FormBuilder)
+  {
+
+
+    this.userForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      parentName: [''],
+
+      mobile: [''],
+      module: [''],
+      roomNumber: [''],
+      blockName: [''],
+
+      country:[''],
+      studentId:['']
+
+     })
+
+  }
+
+ 
+
+  logout():void{
+    this.router.navigate(['/login']);
+  }
+
+
+  ngOnInit(){
+    
+  }
+
 
 }
